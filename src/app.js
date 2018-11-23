eab1390912be968ae19922f5bf6fec7fd80c2fcc
Lucas Borges Fernandes <lucas.fernandes@zup.com.br>
2018-11-23 15:15:52 -0200

    AWS SDK for secrets storage

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import { expressConfig, expressFactory } from './common/config/express';
 import { serverConfig, projectConfig } from './common/config/project';
 import { mongoConnection } from './common/config/mongoose';
+import { awsProvider } from "./common/providers";
 import { router } from './api';
 import http from 'http';
 
@@ -25,5 +26,16 @@ const startServer = () => {
     );
 };
 
-connectMongo(`mongodb://s2m:banana01@localhost/node_playground?authSource=admin`, { useMongoClient: true });
-startServer();
+const onGetMongoCredentialsSuccess = (secret) => {
+    const { username, password, host, database } = secret;
+    connectMongo(`mongodb://${username}:${password}@${host}/${database}?authSource=admin`, { useMongoClient: true });
+    startServer();
+};
+
+const onGetMongoCredentialsError = (error) => {
+    console.info(`There was an error fetching mongo credentials: ${error}`);
+};
+
+awsProvider.getAwsSecret('mongo_local')
+    .then(secret => onGetMongoCredentialsSuccess(secret))
+    .catch(error => onGetMongoCredentialsError(error));
